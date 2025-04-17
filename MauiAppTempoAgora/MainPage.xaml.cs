273cb5b2fa3b0ae834e6420243cb4da7df4bee1c
0200832211038 <0200832211038@LAB03-10>
2025-04-16 21:58:24 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.Diagnostics;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latidude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n";
+                        lbl_res.Text = dados_previsao;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=mm&" +
+                                      $"metricTemp=ºC&metricWind=km/h&zoom=4&overlay=wind&" +
+                                      $"product=ecmwf&level=surface" +
+                                      $"&lat={t.lat.ToString().Replace(",", ".")}" +
+                                      $"&lon={t.lon.ToString().Replace(",", ".")}";
+
+                        wv_mapa.Source = mapa;
+                        Debug.WriteLine(mapa);
+                    } else
+                    {
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+                } else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+            } catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK"); 
+            }
+        }
+    }
+}
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,50 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "6135072afe7f6cec1537d5cb08a5a1a2";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if(resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        lat = Convert.ToDouble(rascunho["coord"]["lat"]),
+                        lon = Convert.ToDouble(rascunho["coord"]["lon"]),
+                        description = rascunho["weather"][0]["description"].ToString(),
+                        main = rascunho["weather"][0]["main"].ToString(),
+                        temp_min = Convert.ToDouble(rascunho["main"]["temp_min"]),
+                        temp_max = Convert.ToDouble(rascunho["main"]["temp_max"]),
+                        speed = Convert.ToDouble(rascunho["wind"]["speed"]),
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString(),
+                        sunset = sunset.ToString(),
+                    };
+                }
+            }
+
+            return t;
+        }
+    }
+}
