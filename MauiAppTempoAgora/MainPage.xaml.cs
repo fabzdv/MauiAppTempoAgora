2ca9247925bf320a40e50927d29cb8f9dabfc616
0200832211038 <0200832211038@LAB03-10>
2025-04-23 21:32:51 -0300

    Finalização do App e Correção de erro

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -11,11 +11,6 @@ namespace MauiAppTempoAgora
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
-        {
-
-        }
-
         private async void Button_Clicked_1(object sender, EventArgs e)
         {
             try
@@ -45,17 +40,85 @@ namespace MauiAppTempoAgora
 
                         wv_mapa.Source = mapa;
                         Debug.WriteLine(mapa);
-                    } else
+                    }
+                    else
                     {
                         lbl_res.Text = "Sem dados de Previsão";
                     }
-                } else
+                }
+                else
                 {
                     lbl_res.Text = "Preencha a cidade.";
                 }
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
-                await DisplayAlert("Ops", ex.Message, "OK"); 
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                GeolocationRequest request = new GeolocationRequest(
+                    GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10)
+                );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+
+                if (local != null)
+                {
+                    string local_disp = $"Latitude: {local.Latitude} \n" +
+                                        $"Longitude: {local.Longitude}";
+                    lbl_coords.Text = local_disp;
+
+                    GetCidade(local.Latitude, local.Longitude);
+                }
+                else
+                {
+                    lbl_coords.Text = "Nenhuma localização.";
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                await DisplayAlert("Erro: Dispositivo não Suporta", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                await DisplayAlert("Erro: Localização Desabilitada", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                await DisplayAlert("Erro: Permissão da Localização", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+
+
+        }
+        private async void GetCidade(double lat, double lon)
+        {
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+
+                Placemark? place = places.FirstOrDefault();
+
+                if (place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Erro: Obtenção do nome da Cidade", ex.Message, "OK");
             }
         }
     }
